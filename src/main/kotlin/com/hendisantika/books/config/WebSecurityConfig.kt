8646898dc60698ab13fce0fa9c6072a288ea064d
hendisantika <hendisantika@yahoo.co.id>
2024-04-06 11:07:18 +0700

    #125 - Bug Fix Security ✏️📗 :octocat: 🐳⬆

--- src/main/kotlin/com/hendisantika/books/config/WebSecurityConfig.kt
+++ src/main/kotlin/com/hendisantika/books/config/WebSecurityConfig.kt
@@ -2,13 +2,13 @@ package com.hendisantika.books.config
 
 import com.hendisantika.books.service.AuthorityUserDetailsService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 
 /**
@@ -17,34 +17,43 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
-class WebSecurityConfig : WebSecurityConfigurerAdapter() {
+//@EnableGlobalSecurity(prePostEnabled = true, securedEnabled = true)
+class WebSecurityConfig {
     @Autowired
     private lateinit var userDetailsService: AuthorityUserDetailsService
 
-    override fun configure(http: HttpSecurity?) {
-        http?: return
-
-        http.authorizeRequests().antMatchers("/css/**").permitAll()
-                .antMatchers("/js/**").permitAll()
-                .antMatchers("/image/**").permitAll()
-                .antMatchers("/signUp").permitAll()
-                .anyRequest().authenticated()
-
-        http.formLogin().loginPage("/login")
-                .loginProcessingUrl("/loginProc")
-                .defaultSuccessUrl("/main")
-                .usernameParameter("loginId")
-                .passwordParameter("password")
-                .failureUrl("/login.html?error=true")
-                .permitAll()
-
-        http.logout().logoutRequestMatcher(AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/login")
-                .deleteCookies("JSESSIONID")
-                .invalidateHttpSession(true)
-                .permitAll()
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
+            // you can either disable this or
+            // put <input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}"/>
+            // inside the login form
+            .csrf { csrf -> csrf.disable() }
+            .authorizeHttpRequests { authz ->
+                authz
+                    .requestMatchers("/css/**", "/img/**", "/js**", "/signUp").permitAll()
+                    .anyRequest().authenticated()
+            }
+            .formLogin { formLogin ->
+                formLogin
+                    .loginPage("/login") // enable this to go to your own custom login page
+                    .loginProcessingUrl("/loginProc") // enable this to use login page provided by spring security
+                    .usernameParameter("loginId")
+                    .passwordParameter("password")
+                    .defaultSuccessUrl("/main", true)
+                    .failureUrl("/login.html?error=true")
+            }
+            .logout { logout ->
+                logout
+                    .logoutRequestMatcher(AntPathRequestMatcher("/logout"))
+                    .logoutSuccessUrl("/login?logout")
+                    .deleteCookies("JSESSIONID")
+                    .invalidateHttpSession(true)
+                    .permitAll()
+            }
+            .build()
     }
+
 
     @Autowired
     fun configAuthentication(auth: AuthenticationManagerBuilder) {
