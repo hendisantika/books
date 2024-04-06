6d00e40d29b3003d45a2644e0271bffdb6c04a89
hendisantika <hendisantika@yahoo.co.id>
2024-04-06 11:11:08 +0700

    #127 - Add Spring Doc libs ✏️📗 :octocat: 🐳⬆

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
 //    implementation("org.flywaydb:flyway-core")
     implementation("org.flywaydb:flyway-mysql")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
     runtimeOnly("com.mysql:mysql-connector-j")
