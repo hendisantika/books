af3a8acd9d0ffa054b6d1db0917f9af9e8f4f2e0
hendisantika <hendisantika@yahoo.co.id>
2024-04-06 10:48:13 +0700

    #123 - Bug Fix Gradle Script ✏️📗 :octocat: 🐳⬆

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,15 +2,18 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("org.springframework.boot") version "3.2.4"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.5.0"
-    kotlin("plugin.spring") version "1.5.0"
-    kotlin("plugin.jpa") version "1.5.0"
+    id("io.spring.dependency-management") version "1.1.4"
+    kotlin("jvm") version "1.9.23"
+    kotlin("plugin.spring") version "1.9.23"
+    kotlin("plugin.jpa") version "1.9.23"
 }
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_21
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+}
+
 
 repositories {
     mavenCentral()
